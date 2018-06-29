1e840591b918e3dba6c87212f7091489c2bbdcb7
Xiaosong Gao <gaoxiaosong06@gmail.com>
2018-06-29 15:23:04 +0800

    添加搜索栏和文本输入框。

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # react-native-hecom-common
+
 公共组件和数据结构。
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+import TextInput from './component/TextInput';
 import FakeSearchBar from './component/FakeSearchBar';
+import SearchBar from './component/SearchBar';
 import { KeyboardAwareListView, KeyboardAwareScrollView, KeyboardAwareFlatList} from 'react-native-keyboard-aware-scroll-view';
 import md5 from 'md5';
 import Toast from 'react-native-root-toast';
 
 export {
+    TextInput,
+    SearchBar,
     FakeSearchBar,
     KeyboardAwareScrollView,
     KeyboardAwareListView,
