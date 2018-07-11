334485e76355748c91fa3cb76e0908ab68ccf936
Xiaosong Gao <gaoxiaosong06@gmail.com>
2018-07-11 15:26:59 +0800

    更新仿搜索栏。

--- src/component/FakeSearchBar.js
+++ src/component/FakeSearchBar.js
@@ -9,52 +9,48 @@ export default class extends React.Component {
         style: PropTypes.any,
     };
 
+    static get defaultProps() {
+        return {
+            activeOpacity: 0.9,
+        };
+    }
+
     render() {
-        const {placeholder, onFocus, style} = this.props;
+        const {placeholder, onFocus, style, activeOpacity} = this.props;
         return (
-            <View style={[styles.containerStyle, style]}>
-                <TouchableOpacity
-                    onPress={onFocus}
-                    style={styles.buttonStyle}
-                    activeOpacity={1}
-                >
-                    <Image
-                        source={require('../image/input_search.png')}
-                        style={styles.image}
-                    />
-                    <Text style={styles.placeholder}>
-                        {placeholder}
-                    </Text>
-                </TouchableOpacity>
-            </View>
+            <TouchableOpacity onPress={onFocus} style={[styles.touch, style]}  activeOpacity={activeOpacity}>
+                <Image
+                    source={require('../image/input_search.png')}
+                    style={styles.image}
+                />
+                <Text style={styles.text} numberOfLines={1}>
+                    {placeholder}
+                </Text>
+            </TouchableOpacity>
         );
     }
 }
 
 const styles = {
-    containerStyle: {
+    touch: {
         flex: 1,
-        backgroundColor: 'white',
-        height: 40,
-    },
-    buttonStyle: {
-        marginLeft: 12,
-        marginRight: 12,
-        height: 32,
-        borderRadius: 4,
-        backgroundColor: '#f5f5f6',
+        height: 35,
+        marginLeft: 16,
+        marginRight: 8,
+        borderRadius: 10,
         flexDirection: 'row',
-        justifyContent: 'flex-start',
         alignItems: 'center',
+        backgroundColor: '#e8e8ea',
     },
     image: {
-        width: 13,
-        height: 13,
         marginLeft: 10,
+        width: 16,
+        height: 16,
     },
-    placeholder: {
-        fontSize: 14,
-        color: '#9c9fa5',
-        marginLeft: 4,
+    text: {
+        marginLeft: 6,
+        fontSize: 15,
+        color: '#999999',
+        flex: 1
     },
 };
