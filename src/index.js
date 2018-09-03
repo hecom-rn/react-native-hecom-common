695b0b1c4c3ce08be61a84baf0db1ff7883a8cd1
Xiaosong Gao <gaoxiaosong06@gmail.com>
2018-09-03 16:54:20 +0800

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import SeperatorSpace from './component/SeperatorSpace';
 import TextInput from './component/TextInput';
 import FakeSearchBar from './component/FakeSearchBar';
 import SearchBar from './component/SearchBar';
-import { KeyboardAwareListView, KeyboardAwareScrollView, KeyboardAwareFlatList} from 'react-native-keyboard-aware-scroll-view';
 import md5 from 'md5';
 import Toast from 'react-native-root-toast';
 import PhotoBrowserPage from './page/PhotoBrowserPage';
@@ -29,9 +28,6 @@ export {
     TextInput,
     SearchBar,
     FakeSearchBar,
-    KeyboardAwareScrollView,
-    KeyboardAwareListView,
-    KeyboardAwareFlatList,
     md5,
     Toast,
     PhotoBrowserPage,
