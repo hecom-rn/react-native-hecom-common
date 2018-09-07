3c99ff9d74cfa714a525818883656f4c1e6ab552
Xiaosong Gao <gaoxiaosong06@gmail.com>
2018-09-07 16:14:04 +0800

    Remove SearchBar and FakeSearchBar to react-native-general-searchbar.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ActivityIndicator from './component/ActivityIndicator';
 import SeperatorLine from './component/SeperatorLine';
 import SeperatorSpace from './component/SeperatorSpace';
 import TextInput from './component/TextInput';
-import FakeSearchBar from './component/FakeSearchBar';
-import SearchBar from './component/SearchBar';
 import md5 from 'md5';
 import Toast from 'react-native-root-toast';
 import PhotoBrowserPage from './page/PhotoBrowserPage';
@@ -26,8 +24,6 @@ export {
     SeperatorLine,
     SeperatorSpace,
     TextInput,
-    SearchBar,
-    FakeSearchBar,
     md5,
     Toast,
     PhotoBrowserPage,
