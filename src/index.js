01764b7c79db89da470fc52d3114f059a361d5cf
Xiaosong Gao <gaoxiaosong06@gmail.com>
2018-11-15 16:10:17 +0800

    移除PhotoBrowserPage。

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ActivityIndicator from './component/ActivityIndicator';
 import SeperatorLine from './component/SeperatorLine';
 import SeperatorSpace from './component/SeperatorSpace';
 import md5 from 'md5';
-import PhotoBrowserPage from './page/PhotoBrowserPage';
 import ActionSheet from './component/ActionSheet/ActionSheet';
 import DropDownFilter from './component/DropDownFilter';
 import ClickableNaviTitle from './component/ClickableNaviTitle';
@@ -20,7 +19,6 @@ export {
     SeperatorLine,
     SeperatorSpace,
     md5,
-    PhotoBrowserPage,
     DropDownFilter,
     ClickableNaviTitle,
     TitleDropDownFilter,
